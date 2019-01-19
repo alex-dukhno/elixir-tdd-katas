d9f62fad54131722d5167f5dfd347994c704f8e1
alex-dukhno <alex.dukhno@icloud.com>
2019-01-19 10:41:27 +0200

    persistent list kata fourth day

---
+++ persistent_list/lib/persistent_list/day04.ex
@@ -0,0 +1,82 @@
+defmodule PersistentList.Day04 do
+  alias PersistentList.Day04, as: List
+
+  defstruct [:head, :tail]
+
+  defimpl String.Chars, for: List do
+    def to_string(list), do: "[" <> stringify(list) <> "]"
+
+    defp stringify(%List{head: nil, tail: nil}), do: ""
+    defp stringify(
+           %List{
+             head: head,
+             tail: %List{
+               head: nil,
+               tail: nil
+             }
+           }
+         ), do: "#{head}"
+    defp stringify(%List{head: head, tail: tail}), do: "#{head}, " <> stringify(tail)
+  end
+
+  def new(), do: %List{}
+
+  def append(list, item), do: %List{head: item, tail: list}
+
+  def prepend(%List{head: nil, tail: nil} = empty, item),
+      do: empty
+          |> append(item)
+  def prepend(%List{head: head, tail: tail}, item),
+      do: tail
+          |> prepend(item)
+          |> append(head)
+
+  def concat(%List{head: nil, tail: nil}, other), do: other
+  def concat(%List{head: head, tail: tail}, other),
+      do: tail
+          |> concat(other)
+          |> append(head)
+
+  def drop(%List{head: nil, tail: nil} = empty, _), do: empty
+  def drop(list, num) when num == 0, do: list
+  def drop(%List{tail: tail}, num),
+      do: tail
+          |> drop(num - 1)
+
+  def drop_while(%List{head: nil, tail: nil} = empty, _), do: empty
+  def drop_while(%List{head: head, tail: tail} = list, predicate), do:
+    if predicate.(head),
+    do:
+      tail
+      |> drop_while(predicate),
+    else:
+      list
+
+  def take(%List{head: nil, tail: nil} = empty, _), do: empty
+  def take(_, num) when num == 0, do: %List{}
+  def take(%List{head: head, tail: tail}, num), do:
+    tail
+    |> take(num - 1)
+    |> append(head)
+
+  def take_while(%List{head: nil, tail: nil} = empty, _), do: empty
+  def take_while(%List{head: head, tail: tail}, predicate), do:
+    if predicate.(head),
+    do:
+      tail
+      |> take_while(predicate)
+      |> append(head),
+    else:
+    %List{}
+
+  def filter(%List{head: nil, tail: nil} = empty, _), do: empty
+  def filter(%List{head: head, tail: tail}, predicate), do:
+    unless predicate.(head),
+    do:
+      tail
+      |> filter(predicate)
+      |> append(head),
+    else:
+      tail
+      |> filter(predicate)
+end
